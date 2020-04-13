863770a4543369ab0b7c1318938764e01a10a8a5
wongiven <xfslove@gmail.com>
2020-04-13 18:51:05 +0800

    update ic -> 2020.1

--- src/main/java/com/github/xfslove/intellij/plugin/redis/action/ExecCommandAction.java
+++ src/main/java/com/github/xfslove/intellij/plugin/redis/action/ExecCommandAction.java
@@ -1,13 +1,6 @@
 package com.github.xfslove.intellij.plugin.redis.action;
 
-import com.github.xfslove.intellij.plugin.redis.CommandRunExecutor;
-import com.github.xfslove.intellij.plugin.redis.storage.Connection;
-import com.github.xfslove.intellij.plugin.redis.ui.ExplorerPanel;
-import com.intellij.execution.RunManager;
-import com.intellij.execution.actions.ConfigurationContext;
-import com.intellij.execution.executors.DefaultRunExecutor;
 import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.actionSystem.CommonDataKeys;
 import com.intellij.openapi.project.DumbAwareAction;
 import org.jetbrains.annotations.NotNull;
 
@@ -19,18 +12,6 @@ public class ExecCommandAction extends DumbAwareAction {
 
   @Override
   public void actionPerformed(@NotNull AnActionEvent e) {
-
-    Connection connection = e.getRequiredData(CommonDataKeys.VIRTUAL_FILE).getUserData(ExplorerPanel.SELECTED_CONFIG);
-
-    CommandRunExecutor executor = CommandRunExecutor.INSTANCE;
-
-    ConfigurationContext context = ConfigurationContext.getFromContext(e.getDataContext());
-
-//    RunnerAndConfigurationSettings runner = producer.findExistingConfigurationWithDefaultEnv(context);
-    RunManager runManager = RunManager.getInstance(e.getProject());
-
-    executor.execute(e.getProject(), context.getConfiguration(), DefaultRunExecutor.getRunExecutorInstance());
-
 
 
   }
